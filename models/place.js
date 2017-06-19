3cfe9a0e75a56d55384545d4cc52d79029dd3db0
kyozone <judolew@gmail.com>
2017-06-19 15:43:03 +0800

    Edited Models

--- models/place.js
+++ models/place.js
@@ -1,26 +1,26 @@
 var mongoose = require("mongoose");
 var bcrypt   = require('bcrypt-nodejs');
 
-var placeSchema = new mongoose.Schema({ 
+var placeSchema = new mongoose.Schema({
 
 	place_id : String,
 	trip : {type: mongoose.Schema.ObjectId, ref: 'Trip'}
 });
 
 
-// MIDDLEWARE TO REMOVE THE REFERENCE TO THE DELETED PLACE FROM THE TRIP'S PLACES ARRAY
+
 
 placeSchema.pre('remove', function(next){
 
     this.model('Trip').update(
-        {_id: this.trip}, 
-        {$pull: {places: this._id}}, 
+        {_id: this.trip},
+        {$pull: {places: this._id}},
         {multi: true},
         function(err, numAffected) {
     		next();
         }
     );
-    
+
 });
 
 
--- models/trip.js
+++ models/trip.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt   = require('bcrypt-nodejs');
 
-var tripSchema = new mongoose.Schema({ 
+var tripSchema = new mongoose.Schema({
 
 	destination: { type: String, required: true },
 	longitude : { type: Number, required: true},
@@ -12,22 +12,16 @@ var tripSchema = new mongoose.Schema({
 });
 
 
-// MIDDLEWARE TO REMOVE THE REFERENCE TO THE DELETED TRIP FROM THE USER'S TRIPS ARRAY
+
 
 tripSchema.pre('remove', function(next){
-
-	console.log("GOT TO LINE 17");
-	console.log(this._id);
-	console.log(this.user);
 
     this.model('User').update(
         {_id: this.user},
         {$pull: {trips: this._id}},
         {multi: true},
         function(err, numAffected) {
-        	console.log("DID IT GET TO HERE? LINE 28");
-    		console.log(err);
-    		console.log(numAffected);
+
     		next()
         }
     );

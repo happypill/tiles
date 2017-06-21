14c462a3d655d93151b1e831cdba74d92df4631b
kyozone <judolew@gmail.com>
2017-06-21 09:51:04 +0800

    Commit Controller

--- models/hasRegisteredBusiness.js
+++
@@ -1,9 +0,0 @@
-// check if the user has registered a business
-module.exports = function (req, res, next) {
-  if (!req.user.business) {
-    req.flash('error', 'You must register a business first (Account > Advanced > Register a business)')
-    res.redirect('/account')
-  } else {
-    next()
-  }
-}
